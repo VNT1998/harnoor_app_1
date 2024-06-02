bde03c91c36af59679d8255b77d7e15ac65440af
VNT1998 <vinitcalpha@gmail.com>
2024-06-02 16:27:27 +0530

    initial commit

---
+++ app/src/main/java/com/vinit/android/app1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vinit.android.app1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_main)
+    }
+}
